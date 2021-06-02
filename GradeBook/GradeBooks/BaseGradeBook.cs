c2d4a81fc834091bdd52203151445ad8976c8e43
JRyGithub <jryland@protonmail.com>
2021-06-02 14:35:49 +0200

    Last bit

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -107,18 +109,24 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            double grade = 0;
+
+            if (studentType == StudentType.DualEnrolled || studentType == StudentType.Honors)
+            {
+                grade += 1;
+            }
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return grade+4;
                 case 'B':
-                    return 3;
+                    return grade+3;
                 case 'C':
-                    return 2;
+                    return grade+2;
                 case 'D':
-                    return 1;
+                    return grade+1;
                 case 'F':
-                    return 0;
+                    return grade;
             }
             return 0;
         }
